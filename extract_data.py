129110f68a1ba7fe6886acd5171f8607c8998a6b
matheus0312 <matheusoliveiradasilva@gmail.com>
2021-09-18 11:10:59 -0300

    Created function to identify climbs. Added calculation of number of POI to insert data in output file Added encoding of

--- extract_data.py
+++ extract_data.py
@@ -1,17 +1,110 @@
 from geopy import distance
 #import requests
 #import pandas as pd
+import math
 
+def safediv(num,den):
+    if den == 0:
+        if num == 0:
+            return 0
+        elif num >0:
+            return 9999
+        else:
+            return -9999
+    else:
+        return num/den
+
+def add_poi_to_instructions(instruction_distance,instruction_data,points_of_interest,name_data):
+    poi_name=points_of_interest[0][0]
+    poi_type=points_of_interest[0][1]
+    poi_distance=points_of_interest[0][2]
+    poi_identification=points_of_interest[0][3]
+
+    for i in range(0,len(poi_name)):
+        instruction_data.append(poi_type)
+        instruction_distance.append(poi_distance)
+        name_data.append(poi_name)
+
+
+    return [instruction_data,instruction_distance,name_data,poi_identification]
 
 #work in progress
 def add_poi_by_climb(points_of_interest, point_attribute):
     distance_from_last_point=point_attribute[0]
     altitude_from_last_point=point_attribute[1]
 
-    grade = []
-    for i in range (0, len(distance_from_last_point)-1):
-        grade.append((altitude_from_last_point[i+1]-altitude_from_last_point[i])/(distance_from_last_point[i+1]-distance_from_last_point[i])*100)
-    grade.append(0)
+    #for i in range(0,len(distance_from_last_point)):
+     #   print(distance_from_last_point[i],altitude_from_last_point[i])
+    poi_name = []
+    poi_type = []
+    poi_distance = []
+    poi_identification = []
+
+
+    mountain_start = 0
+    mountain_finish = 0
+    mountain = []
+    #grade = []
+    #for i in range (0, len(distance_from_last_point)-1):
+    #    grade.append((altitude_from_last_point[i+1]-altitude_from_last_point[i])/(distance_from_last_point[i+1]-distance_from_last_point[i])*100)
+    #grade.append(0)
+
+    distance_from_start = [0.0]
+
+    # creates array indicating distance from start for each point
+    for i in range (1,len(distance_from_last_point)):
+        distance_from_start.append(distance_from_start[i-1]+distance_from_last_point[i])
+
+
+    i= 0
+    while i < len(distance_from_last_point):
+        in_mountain=False
+        current_distance = 0
+        current_delta_altitude = 0
+        for j in range(i+1, len(distance_from_last_point)):
+            current_distance += distance_from_last_point[j]
+            current_delta_altitude += altitude_from_last_point[j]
+            if current_distance >= 500:
+                grade = (current_delta_altitude / current_distance) * 100
+                if (grade >= 3) and (safediv(altitude_from_last_point[j],distance_from_last_point[j])*100>-2):
+                    climb_score = grade * current_distance
+                    if climb_score >= 3500:
+                        if not in_mountain:
+                            mountain_start_point = i
+                            mountain_start_distance = distance_from_start[i]
+                            in_mountain=True
+                    else:
+                        if in_mountain:
+                            mountain_finish_point = j
+                            mountain_finish_distance = distance_from_start[j]
+
+                        break
+                else:
+                    if in_mountain:
+                        mountain_finish_point = j
+                        mountain_finish_distance = distance_from_start[j]
+
+                    break
+        i += 1
+        if in_mountain:
+            poi_name.append('st '+str(int(grade))+'% '+str(int(float((mountain_finish_distance-mountain_start_distance)/1000.0)))+'km')
+            poi_type.append(b'\x65')
+            poi_distance.append(mountain_start_distance)
+            poi_identification.append(mountain_start_point)
+
+            print('st '+str(int(grade))+'% '+str(int(float((mountain_finish_distance-mountain_start_distance)/1000.0)))+'km')
+
+            poi_name.append('end' + str(int(current_delta_altitude))+ 'm ' + str(int(climb_score))+'sc')
+            poi_type.append(b'\x65')
+            poi_distance.append(mountain_finish_distance)
+            poi_identification.append(mountain_finish_point)
+
+            print('end' + str(int(current_delta_altitude))+ 'm ' + str(int(climb_score))+'sc')
+
+            i = j
+
+
+    points_of_interest = [poi_name,poi_type,poi_distance,poi_identification]
 
     return points_of_interest
 
@@ -24,15 +117,16 @@ def add_poi_by_distance(points_of_interest, point_attribute,inserted_poi):
     poi_name = inserted_poi [0]
     poi_type = inserted_poi [1]
     poi_distance = inserted_poi [2]
+    poi_identification = 0
     distance_from_start=0
     for i in range(0, len(point_attribute)):
         distance_from_start += distance_from_last_point[i]
-        if poi_distance > distance_from_start:
+        if float(poi_distance) < distance_from_start:
             poi_identification = i-1
             poi_distance = distance_from_start - distance_from_last_point[i]
             break
 
-    points_of_interest.append([poi_name,poi_type,poi_distance])
+    points_of_interest.append([poi_name,poi_type,poi_distance,poi_identification])
 
     return points_of_interest
 
@@ -43,13 +137,13 @@ def calculate_points_attributes(latitude_data,longitude_data,altitude_data):
     delta_altitude_from_last_point = []
 
     distance_from_last_point.append(0)
-    delta_altitude_from_last_point(0)
+    delta_altitude_from_last_point.append(0)
 
     for i in range(1, len(latitude_data)):
         current_point=(latitude_data[i] / 1000000, longitude_data[i] / 1000000)
         last_point=(latitude_data[i - 1] / 1000000, longitude_data[i - 1] / 1000000)
         distance_from_last_point.append(distance.distance(current_point, last_point).m)
-        delta_altitude_from_last_point.append(altitude_data[i] - altitude_data[i-1])
+        delta_altitude_from_last_point.append((altitude_data[i] - altitude_data[i-1]))
 
     point_attribute = [distance_from_last_point,delta_altitude_from_last_point]
 
@@ -105,7 +199,7 @@ def add_altitude_data(decoded_data):
     decoded_data = [latitude_data, longitude_data, altitude_data, instruction_data]
     return decoded_data
 
-def calculate_number_data(instructions_data):
+def calculate_number_data(instructions_data,points_of_interest):
 
     number_points = len(instructions_data)
     number_instructions = 0
@@ -113,7 +207,10 @@ def calculate_number_data(instructions_data):
         if instruction_point != b'\xff':
             number_instructions += 1
 
-    number_data = [number_points, number_instructions]
+    number_pois = len(points_of_interest[0])
+
+
+    number_data = [number_points, number_instructions,number_pois]
     return number_data
 
 def calculate_alt_bounding_box(altitude_data):
@@ -167,7 +264,9 @@ def extract_attributes(decoded_data,inserted_poi):
     longitude_data = decoded_data[1]
     altitude_data = decoded_data[2]
     instruction_data = decoded_data[3]
+    name_data = decoded_data[4]
     points_of_interest = []
+
 
     lat_lon_bounding_box = calculate_lat_lon_bounding_box(latitude_data, longitude_data)
 
@@ -175,16 +274,19 @@ def extract_attributes(decoded_data,inserted_poi):
 
     alt_bounding_box = calculate_alt_bounding_box(altitude_data)
 
-    number_data = calculate_number_data(instruction_data)
-
     instruction_distance = calculate_instruction_distance(instruction_data,latitude_data,longitude_data)
 
     point_attribute = calculate_points_attributes(latitude_data,longitude_data,altitude_data)
 
-    points_of_interest = add_poi_by_distance(points_of_interest, point_attribute,inserted_poi)
+    #points_of_interest = add_poi_by_distance(points_of_interest, point_attribute,inserted_poi)
 
     points_of_interest = add_poi_by_climb(points_of_interest, point_attribute)
 
-    extracted_attributes = [lat_lon_bounding_box,total_distance,alt_bounding_box,number_data,instruction_distance]
+    number_data = calculate_number_data(instruction_data,points_of_interest)
 
-    return  extracted_attributes
+    decoded_data = [latitude_data,longitude_data,altitude_data,instruction_data,name_data]
+
+    extracted_attributes = [lat_lon_bounding_box,total_distance,alt_bounding_box,number_data,instruction_distance,points_of_interest]
+
+    return  [extracted_attributes,decoded_data]
+
