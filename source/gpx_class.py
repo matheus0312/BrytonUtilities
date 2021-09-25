3d0c101f5835811be598a8fdf73ccfd205584d86
matheus0312 <matheusoliveiradasilva@gmail.com>
2021-09-24 23:11:39 -0300

    Finish gpx decoding
    Finished first draft of GPX decoding, comparison with

--- source/gpx_class.py
+++ source/gpx_class.py
@@ -1,3 +1,5 @@
+import point_class
+
 class Gpx:
 
 
@@ -25,12 +27,54 @@ class Gpx:
         # breaks single line file in multiple lines
         self.__gpx_lines = self.__gpx_lines.split('<')
 
+    def __set_latitude(self,point):
+
+        if self.__line.find('lat=') != -1:
+            point
+
+    def __search_for_lat_lon(self):
+        if self.__line.find('lat=') != -1:
+            self.__point.set_latitude(self.__line.split('"')[1])
+            self.__point.set_longitude(self.__line.split('"')[3])
+            self.__number_of_items += 1
+
+    def __search_for_alt(self):
+        if self.__line.find('<ele>') != -1 and self.__number_of_items > 0:
+            self.__point.set_altitude(self.__line.lstrip().removeprefix('<ele>'))
+            self.__number_of_items += 1
+
+    def __search_for_ins(self):
+        if self.__line.find('<type>') != -1 and self.__number_of_items > 0:
+            self.__point.set_instruction(int(self.__line.lstrip().removeprefix('<type>')))
+            self.__number_of_items += 1
+
+    def __search_for_nam(self):
+        if self.__line.find('<name>') != -1 and self.__number_of_items > 0:
+            self.__point.set_name(self.__line.strip().removeprefix('<name>'))
+            self.__number_of_items += 1
+
+    def __point_is_complete(self):
+        if self.__number_of_items%5 == 0:
+            return True
+        else:
+            return False
 
     def __decode_ors(self):
 
         self.__open_ors()
 
+        self.__point = point_class.Point()
+        self.__points = []
+        self.__number_of_items = 0
+
         for self.__line in self.__gpx_lines:
             self.__line = '<' + self.__line
 
-            # work in progress
+            self.__search_for_lat_lon()
+            self.__search_for_alt()
+            self.__search_for_ins()
+            self.__search_for_nam()
+
+            if self.__point_is_complete():
+                self.__points.append(self.__point)
+
--- source/point_class.py
+++ source/point_class.py
@@ -1,7 +1,19 @@
 class Point:
 
-    def __init__(self,latitude,longitude,altitude,instruction='none'):
+    def __init__(self):
+        pass
+
+    def set_latitude(self, latitude):
         self.__latitude = latitude
+
+    def set_longitude(self, longitude):
         self.__longitude = longitude
+
+    def set_altitude(self, altitude):
         self.__altitude = altitude
+
+    def set_instruction(self, instruction):
         self.__instruction = instruction
+
+    def set_name(self, name):
+        self.__name = name
